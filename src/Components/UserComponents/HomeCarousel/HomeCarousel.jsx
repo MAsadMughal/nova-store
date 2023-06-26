82345de11d0b0b0fab220ffd81e61af86dea4435
Asadullah38 <2020se10@student.uet.edu.pk>
2023-06-26 13:31:47 +0500

    finals

--- src/Components/UserComponents/HomeCarousel/HomeCarousel.jsx
+++ src/Components/UserComponents/HomeCarousel/HomeCarousel.jsx
@@ -7,21 +7,21 @@ import "./Carousel.scss"
 const HomeCarousel = () => {
   return (
     <div id='carousel-main' className='mb-5'>
-      <div id="carouselExampleIndicators" className="carousel slide" style={{ height: window.innerWidth>=500?'50vh':'30vh', marginTop: '0px' }} data-bs-ride="carousel" data-bs-interval='1000'>
+      <div id="carouselExampleIndicators" className="carousel slide" style={{ height: window.innerWidth >= 576 ? '50vh' : '30vh', marginTop: '0px' }} data-bs-ride="carousel" data-bs-interval='1000'>
         <div className="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
-        <div className="carousel-inner" >
+        <div className="carousel-inner w-100" >
           <div className="carousel-item  active">
-            <img style={{ height: window.innerWidth>=500?'50vh':'30vh' }} alt='carousel-Img' src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <img style={{ objectFit:'cover', maxHeight: window.innerWidth >= 576 ? '50vh' : '30vh', }} alt='carousel-Img' src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
           </div>
           <div className="carousel-item ">
-            <img style={{ height: window.innerWidth>=500?'50vh':'30vh' }} alt='carousel-Img2' src="https://images.pexels.com/photos/7349260/pexels-photo-7349260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <img style={{ objectFit:'cover', maxHeight: window.innerWidth >= 576 ? '50vh' : '30vh', }} alt='carousel-Img2' src="https://images.pexels.com/photos/7349260/pexels-photo-7349260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
           </div>
           <div className="carousel-item ">
-            <img style={{ height: window.innerWidth>=500?'50vh':'30vh' }} alt='carousel-Img3' src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <img style={{ objectFit:'cover', maxHeight: window.innerWidth >= 576 ? '50vh' : '30vh', }} alt='carousel-Img3' src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
           </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
